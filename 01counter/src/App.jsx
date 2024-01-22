095285162d6f0ace2b65db5058415d9b0bad283c
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-01-22 20:55:36 +0530

    first commit

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+ //let counter = 10
+
+  const addValue = () =>{
+    //console.log("value added", counter)
+    //counter = counter + 1
+    if(counter < 20)
+    {
+      counter = counter + 1
+    }
+    else{
+      counter = 20
+    }
+    setCounter(counter)
+  }
+  const minusValue = () =>{
+    //console.log("value deducted", counter)
+    //counter = counter - 1
+    //here i have used 2 methods to update value in these functions for better understanding of the concepts
+    if(counter > 0){
+      //counter = counter - 1
+      setCounter(--counter)
+    }
+    else{
+      counter = 0
+    }
+    //setCounter(counter)
+  }
+
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button 
+        onClick = {addValue}
+      >Increse Value</button>
+
+      <button onClick = {minusValue} 
+      >Decrease Value</button>
+
+    </>
+  )
+}
+
+export default App
